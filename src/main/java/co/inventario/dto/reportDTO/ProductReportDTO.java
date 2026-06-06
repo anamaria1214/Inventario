a2516f6c1d3031d5014cdb4585b330f50a16904f
unknown <florezvasquezjuanmiguel@gmail.com>
2026-06-06 12:30:43 -0500

    fix: amount bug in ReportService

--- src/main/java/co/inventario/dto/reportDTO/ProductReportDTO.java
+++ src/main/java/co/inventario/dto/reportDTO/ProductReportDTO.java
@@ -2,5 +2,5 @@ package co.inventario.dto.reportDTO;
 
 import java.time.LocalDateTime;
 
-public record ProductReportDTO(LocalDateTime buyDate, String name, int amount, Double precioUnidad, Double precioTotal) {
+public record ProductReportDTO(LocalDateTime buyDate, String name, Double amount, Double precioUnidad, Double precioTotal) {
 }
